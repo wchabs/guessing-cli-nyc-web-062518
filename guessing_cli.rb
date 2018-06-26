cf6f79d2ce811b684ab8489f47aaef4aeee9cae5
William Chabla <w_chabla@hotmail.com>
2018-06-26 21:30:15 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -15,8 +15,7 @@ def run_guessing_game
     puts "Try again or type 'exit'"
     reply = gets.chomp
     if reply == "exit"
-      puts "
-      Goodbye!"
+      puts "Goodbye!"
       return
     else
       run_guessing_game
